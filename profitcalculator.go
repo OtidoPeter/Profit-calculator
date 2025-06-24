b12db3392feb2461aa1e97e637d9599f9538ba09
Otido Peter <peterotido@gmail.com>
2025-06-24 11:16:42 +0300

    operations function

--- profitcalculator.go
+++ profitcalculator.go
@@ -16,15 +16,31 @@ func main() {
 	outputText("Tax Rate: ")
 	fmt.Scan(&taxRate)
 
-	ebt := revenue - expenses
+	operations(revenue, expenses, taxRate)
+	/*ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+	*/
 }
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func operations(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	reformattedEbt := fmt.Sprintf("Earnings before tax: %.2f\n", ebt)
+	reformattedProfit := fmt.Sprintf("Earnings after tax: %.2f\n", profit)
+	reformattedRatio := fmt.Sprintf("Ratio: %.2f\n", ratio)
+
+	fmt.Print(reformattedEbt, reformattedProfit, reformattedRatio)
+
+	return ebt, profit, ratio
 }
